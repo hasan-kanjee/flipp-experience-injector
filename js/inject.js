c20023d89a9ac6a83a54fc146034245e984237ae
Hasan Kanjee <hasankanjee@flipp.com>
2023-01-18 10:46:56 -0500

    initial commit

---
+++ js/inject.js
@@ -0,0 +1,18 @@
+// var html = `<div class="iframe-container" id="flipp-container">
+// <div id="flipp-scroll-ad-content">
+//   <div style="font-size: 20px; background-color: green; height: 100px;">
+//     WILL BE INJECTED HERE!
+//   </div>
+// </div>
+// </div>`
+
+// document.body.addEventListener("click", async function (evt) {
+//   var e = document.getElementById("flipp-container");
+//   if (e) {
+//     e.remove();
+//   }
+//   console.dir(this);
+//   //note evt.target can be a nested element, not the body element, resulting in misfires
+//   console.log(evt.target);
+//   evt.target.insertAdjacentHTML("afterEnd", html)
+// });
